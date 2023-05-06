393542583842677d9871321ca8e3f7e45de6d7ab
Adam Volkman <itismaku@gmail.com>
2023-05-06 16:13:40 +0200

    feat(qb): prepare for qb impl

--- modules/framework/type/qb/client.lua
+++ modules/framework/type/qb/client.lua
@@ -1,30 +1,9 @@
 local client = {}
 
 client.openMenu = function(id, title, content)
-    local elements = {}
-    for _, element in ipairs(content) do
-        table.insert(elements, { label = element.name, value = element.id })
-    end
-    esx.UI.Menu.Open('default', GetCurrentResourceName(), id, {
-        title = title,
-        align = 'right',
-        elements = elements
-    }, function(data, menu)
-        for _, element in ipairs(content) do
-            if element.id == data.current.value then
-                if element.callback() then
-                    menu.close()
-                end
-                break
-            end
-        end
-    end, function(data, menu)
-        menu.close()
-    end)
 end
 
 client.showHelpNotification = function(text)
-    esx.ShowHelpNotification(text)
 end
 
 return client
--- modules/framework/type/qb/server.lua
+++ modules/framework/type/qb/server.lua
@@ -1,1 +1,21 @@
 local server = {}
+
+server.getMoney = function(playerId, type)
+end
+
+server.removeMoney = function(playerId, type, amount)
+end
+
+server.addMoney = function(playerId, type, amount)
+end
+
+server.getCharacterName = function(identifier)
+end
+
+server.getIdentifier = function(playerId)
+end
+
+server.sendNotification = function(playerId, type, message)
+end
+
+return server
